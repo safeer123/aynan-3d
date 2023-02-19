8e16bd84db052a147dd3e78821ed2a15c0cc6fac
Safeer Chonengal <safeer2c@gmail.com>
2023-02-19 19:39:16 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+import PhotoUploader from "./components/photo-uploader";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Aynan</h1>
+      <h2>Build 3D content on web</h2>
+
+      <PhotoUploader />
+    </div>
+  );
+}
