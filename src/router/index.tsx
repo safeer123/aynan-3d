1a854356ff6046e90e8608b617d809320fe47af0
Safeer Chonengal <safeer@drivetrain.ai>
2023-03-21 08:40:57 +0530

    router not working in github pages deployment

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import AnaglyphMaker from '../pages/anaglyph-maker';
 import Home from '../pages/home';
 import { Routes } from './constants';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Home />,
