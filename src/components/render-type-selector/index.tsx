066a78826a44959c8b093c32307f71f9ea05f56f
Safeer Chonengal <safeer@drivetrain.ai>
2023-03-23 21:12:55 +0530

    render selection border added. #23

--- src/components/render-type-selector/index.tsx
+++ src/components/render-type-selector/index.tsx
@@ -2,8 +2,18 @@ import { Segmented } from 'antd';
 import { BlockOutlined, PictureOutlined } from '@ant-design/icons';
 import { SegmentedValue } from 'antd/es/segmented';
 import { useContext } from 'react';
+import styled from 'styled-components';
 import { RenderType } from '../../types/render';
 import { AnaglyphTBContext, AnaglyphTBContextType } from '../../contexts/anaglyphToolboxContext';
+
+const StyledSegmented = styled(Segmented)`
+  .ant-segmented-item {
+    border: 1px solid transparent;
+  }
+  .ant-segmented-item-selected {
+    border: 1px solid #b0b0b0;
+  }
+`;
 
 const RenderTypeSelector = () => {
   const { controlValues, updateControlValues } = useContext(
@@ -15,7 +25,7 @@ const RenderTypeSelector = () => {
     });
   };
   return (
-    <Segmented
+    <StyledSegmented
       onChange={onChangeType}
       value={controlValues?.selectedRenderType}
       options={[
